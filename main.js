597f261d3ca4cc8c420b69564c13a8f7f425e9f8
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-03-31 21:27:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+    canvas = createCanvas(350,300);
+    canvas.position(450,200);
+    video = createCapture(VIDEO);
+    video.hide();
+  }
+  function draw(){
+    image(video,0,0,350,300);
+  }
+  
+  
+  
+  
