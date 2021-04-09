e3560e405b9486f193221f0d08581638c58dc00b
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-04-09 21:34:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
   function setup() {
-    classifier1=ml5.imageClassifier('MobileNet',modelLoaded1);
-    classifier2=ml5.imageClassifier('Google Lens',modelLoaded2);
+    canvas = createCanvas(350,300);
+    canvas.position(500,200);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('MobileNet',modelLoaded1);
   }
   function modelLoaded1(){
     console.log("It WORKED");
+  
   }
-  function modelLoaded2(){
-    console.log("It WORKED");
-  }
-  7="download.jpg"
   function draw(){
-    classifier1.classify(7,gotResults1);
-    classifier2.classify(7,gotResults2);
+    classifier.classify(video,gotResults1);
   }
-  function gotResults1(){
- 
-      console.log(results);
-      document.getElementById("3").innerHTML=results[0].label;
-
-    
-  }
-  function gotResults2(){
-
-      console.log(results);
-      document.getElementById("2").innerHTML=results[0].label;
-
+  function gotResults1(error,results){
+    if(error){
+      console.error(error);
     }
-   
+    else{
+      console.log(results);
+  }
+}
